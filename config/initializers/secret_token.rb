62c1af5486ecffb08a16d322caa54eb0b3a65daa
nyros <nyros@nyros-desktop.(none)>
2013-03-18 15:37:21 +0530

    test commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pg::Application.config.secret_token = 'f5fdb9b7364070dcae8e47d73cc6571d84f2e47f1d661a40b9343f6f0d05e062912fe010a6b6f3d7c324e032afa0ca2ae648a680d8f677a9bebf5242bfbf6622'
